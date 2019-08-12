10937ac3aa97d8c2312b7a521982d36aeb871f1b
AntaresT <luizeduardovbe@gmail.com>
2019-08-12 20:30:46 -0300

    First Commit

---
+++ todo.py
@@ -0,0 +1,9 @@
+# Lista de Afazeres
+
+class todo():
+
+    def __init__(self, nomeTarefa, descricao, status ):
+        self.nomeTarefa = nomeTarefa
+        self.descricao = descricao
+        self.status = status
+
