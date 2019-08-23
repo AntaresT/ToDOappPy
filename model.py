d28a98c2f7c8a0af4dcab7d8f9789f6a636ddb39
AntaresT <luizeduardovbe@gmail.com>
2019-08-22 22:54:01 -0300

    Ajuste de BD

--- model.py
+++ model.py
@@ -1,19 +1,25 @@
+# -*- coding: utf-8 -*-
 from sqlalchemy import Column, String, Integer, Date
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+# -*- usado para criar a conexão com o banco -*-
 engine = create_engine("sqlite:///para_fazer.db")
 session = sessionmaker(bind=engine)()
 
+"""usado para mapear as classe que serão tabelas"""
 Base = declarative_base()
 
 class ParaFazer(Base):
+
+    __tablename__ = "ParaFazer"
+
     id          = Column(Integer, primary_key=True)
     titulo      = Column(String(50))
     descricao   = Column(String(200))
     dt_criacao  = Column(Date)
-    concluido   = Column(Integer(1))
+    concluido   = Column(Integer)
 
     def __init__(self,titulo,descricao,dt_criacao,concluido):
         self.titulo     = titulo
@@ -21,4 +27,3 @@ class ParaFazer(Base):
         self.dt_criacao = dt_criacao
         self.concluido  = concluido 
 
-lista = ParaFazer()
